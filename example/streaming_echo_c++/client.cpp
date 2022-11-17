7cef36a9e4419c3a02d075afc2edd544e7b3779a
Sheng-Liang Song <shengliang.song@bytedance.com>
2022-11-17 20:56:13 +0000

    Update stream test

--- example/streaming_echo_c++/client.cpp
+++ example/streaming_echo_c++/client.cpp
@@ -17,67 +17,96 @@
 
 // A client sending requests to server in batch every 1 second.
 
-#include <gflags/gflags.h>
-#include <butil/logging.h>
+#include "echo.pb.h"
 #include <brpc/channel.h>
 #include <brpc/stream.h>
-#include "echo.pb.h"
+#include <butil/logging.h>
+#include <gflags/gflags.h>
+#include <numeric>
 
 DEFINE_bool(send_attachment, true, "Carry attachment along with requests");
-DEFINE_string(connection_type, "", "Connection type. Available values: single, pooled, short");
+DEFINE_string(connection_type, "",
+              "Connection type. Available values: single, pooled, short");
 DEFINE_string(server, "0.0.0.0:8001", "IP Address of server");
 DEFINE_int32(timeout_ms, 100, "RPC timeout in milliseconds");
-DEFINE_int32(max_retry, 3, "Max retries(not including the first RPC)"); 
+DEFINE_int32(max_retry, 3, "Max retries(not including the first RPC)");
 
-int main(int argc, char* argv[]) {
-    // Parse gflags. We recommend you to use gflags as well.
-    GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
+int main(int argc, char *argv[]) {
+  // Parse gflags. We recommend you to use gflags as well.
+  GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
 
-    // A Channel represents a communication line to a Server. Notice that 
-    // Channel is thread-safe and can be shared by all threads in your program.
-    brpc::Channel channel;
-        
-    // Initialize the channel, NULL means using default options. 
-    brpc::ChannelOptions options;
-    options.protocol = brpc::PROTOCOL_BAIDU_STD;
-    options.connection_type = FLAGS_connection_type;
-    options.timeout_ms = FLAGS_timeout_ms/*milliseconds*/;
-    options.max_retry = FLAGS_max_retry;
-    if (channel.Init(FLAGS_server.c_str(), NULL) != 0) {
-        LOG(ERROR) << "Fail to initialize channel";
-        return -1;
-    }
+  // A Channel represents a communication line to a Server. Notice that
+  // Channel is thread-safe and can be shared by all threads in your program.
+  brpc::Channel channel;
 
-    // Normally, you should not call a Channel directly, but instead construct
-    // a stub Service wrapping it. stub can be shared by all threads as well.
-    example::EchoService_Stub stub(&channel);
-    brpc::Controller cntl;
-    brpc::StreamId stream;
-    if (brpc::StreamCreate(&stream, cntl, NULL) != 0) {
-        LOG(ERROR) << "Fail to create stream";
-        return -1;
-    }
-    LOG(INFO) << "Created Stream=" << stream;
-    example::EchoRequest request;
-    example::EchoResponse response;
-    request.set_message("I'm a RPC to connect stream");
-    stub.Echo(&cntl, &request, &response, NULL);
-    if (cntl.Failed()) {
-        LOG(ERROR) << "Fail to connect stream, " << cntl.ErrorText();
-        return -1;
-    }
-    
-    while (!brpc::IsAskedToQuit()) {
-        butil::IOBuf msg1;
-        msg1.append("abcdefghijklmnopqrstuvwxyz");
-        CHECK_EQ(0, brpc::StreamWrite(stream, msg1));
-        butil::IOBuf msg2;
-        msg2.append("0123456789");
-        CHECK_EQ(0, brpc::StreamWrite(stream, msg2));
-        sleep(1);
-    }
+  // Initialize the channel, NULL means using default options.
+  brpc::ChannelOptions options;
+  options.protocol = brpc::PROTOCOL_BAIDU_STD;
+  options.connection_type = FLAGS_connection_type;
+  options.timeout_ms = FLAGS_timeout_ms /*milliseconds*/;
+  options.max_retry = FLAGS_max_retry;
+  if (channel.Init(FLAGS_server.c_str(), NULL) != 0) {
+    LOG(ERROR) << "Fail to initialize channel";
+    return -1;
+  }
 
-    CHECK_EQ(0, brpc::StreamClose(stream));
-    LOG(INFO) << "EchoClient is going to quit";
+  // Normally, you should not call a Channel directly, but instead construct
+  // a stub Service wrapping it. stub can be shared by all threads as well.
+  example::EchoService_Stub stub(&channel);
+  brpc::Controller cntl;
+
+  brpc::StreamId stream;
+  if (brpc::StreamCreate(&stream, cntl, NULL) != 0) {
+    LOG(ERROR) << "Fail to create stream";
+    return -1;
+  }
+  LOG(INFO) << "Created Stream=" << stream;
+  example::EchoRequest request;
+  example::EchoResponse response;
+  request.set_message("I'm a RPC to connect stream");
+  stub.Echo(&cntl, &request, &response, NULL);
+  if (cntl.Failed()) {
+    LOG(ERROR) << "Fail to connect stream, " << cntl.ErrorText();
+    return -1;
+  }
+
+  uint64_t LSN = 0ULL;
+
+  auto streaming = [&](void const *msg, uint32_t len) -> int {
+    butil::IOBuf pkt;
+    pkt.append(msg, 4);
+    pkt.append(&len, sizeof(uint32_t));
+    pkt.append(&LSN, sizeof(uint64_t));
+    uint32_t unit_size = 1024;
+    while (!brpc::IsAskedToQuit() && len > 0) {
+      uint32_t sz = std::min(len, unit_size);
+      pkt.append(msg, sz);
+      len -= sz;
+    }
+    int rc = 0;
+    do {
+      if (rc == EAGAIN)
+        sleep(0.1);
+      rc = brpc::StreamWrite(stream, pkt);
+    } while (rc == EAGAIN);
     return 0;
+  };
+
+  char page[1024 * 4 * 4];
+  // std::iota(std::begin(page), std::end(page), 0);
+  for (int i = 0; i < 16 * 1024; ++i)
+    page[i] = 'A';
+  uint32_t len = 0;
+  while (!brpc::IsAskedToQuit()) {
+    ++LSN;
+    uint32_t sz = std::min(len, (uint32_t)sizeof(page));
+    streaming(page, sz);
+    sleep(0.2);
+    ++len;
+    len %= 1024;
+  }
+
+  CHECK_EQ(0, brpc::StreamClose(stream));
+  LOG(INFO) << "EchoClient is going to quit";
+  return 0;
 }
--- example/streaming_echo_c++/server.cpp
+++ example/streaming_echo_c++/server.cpp
@@ -17,99 +17,127 @@
 
 // A server to receive EchoRequest and send back EchoResponse.
 
-#include <gflags/gflags.h>
-#include <butil/logging.h>
-#include <brpc/server.h>
 #include "echo.pb.h"
+#include <brpc/server.h>
 #include <brpc/stream.h>
+#include <butil/logging.h>
+#include <gflags/gflags.h>
 
 DEFINE_bool(send_attachment, true, "Carry attachment along with response");
 DEFINE_int32(port, 8001, "TCP Port of this server");
-DEFINE_int32(idle_timeout_s, -1, "Connection will be closed if there is no "
+DEFINE_int32(idle_timeout_s, -1,
+             "Connection will be closed if there is no "
              "read/write operations during the last `idle_timeout_s'");
-DEFINE_int32(logoff_ms, 2000, "Maximum duration of server's LOGOFF state "
+DEFINE_int32(logoff_ms, 2000,
+             "Maximum duration of server's LOGOFF state "
              "(waiting for client to close connection before server stops)");
 
 class StreamReceiver : public brpc::StreamInputHandler {
 public:
-    virtual int on_received_messages(brpc::StreamId id, 
-                                     butil::IOBuf *const messages[], 
-                                     size_t size) {
-        std::ostringstream os;
-        for (size_t i = 0; i < size; ++i) {
-            os << "msg[" << i << "]=" << *messages[i];
-        }
-        LOG(INFO) << "Received from Stream=" << id << ": " << os.str();
-        return 0;
-    }
-    virtual void on_idle_timeout(brpc::StreamId id) {
-        LOG(INFO) << "Stream=" << id << " has no data transmission for a while";
-    }
-    virtual void on_closed(brpc::StreamId id) {
-        LOG(INFO) << "Stream=" << id << " is closed";
+  virtual int on_received_messages(brpc::StreamId id,
+                                   butil::IOBuf *const messages[],
+                                   size_t size) {
+    if (size == 0)
+      return 0;
+    auto *iobuf = messages[0];
+    uint64_t LSN = 0ULL;
+    uint32_t len = 0;
+    int n = 0;
+    char code[5];
+    code[4] = '\0';
+    if (iobuf->size() >= 16) {
+      n = iobuf->cutn(code, 4);
+      n += iobuf->cutn(&len, 4);
+      n += iobuf->cutn(&LSN, 8);
     }
 
+    size_t i = 0;
+    int data_size = 0;
+    std::string msg;
+    uint32_t curr_len = len;
+    while (i < size && iobuf->size() > 0 && curr_len > 0) {
+      iobuf = messages[i];
+      uint32_t sz = std::min(curr_len, 128U);
+      std::string bufstr;
+      uint32_t cp_sz = iobuf->cutn(&bufstr, sz);
+      msg += bufstr;
+      data_size += cp_sz;
+      if (iobuf->size() == 0) {
+        ++i;
+      }
+      curr_len -= cp_sz;
+    }
+
+    LOG(INFO)
+        << "Received from Stream=" << id << ": " << size << " LSN:" << LSN
+        << " len:" << len << " curr_len:"
+        << curr_len; // << " n:" << n << " " << code << ":[" << msg << "]";
+    return 0;
+  }
+  virtual void on_idle_timeout(brpc::StreamId id) {
+    LOG(INFO) << "Stream=" << id << " has no data transmission for a while";
+  }
+  virtual void on_closed(brpc::StreamId id) {
+    LOG(INFO) << "Stream=" << id << " is closed";
+  }
 };
 
 // Your implementation of example::EchoService
 class StreamingEchoService : public example::EchoService {
 public:
-    StreamingEchoService() : _sd(brpc::INVALID_STREAM_ID) {};
-    virtual ~StreamingEchoService() {
-        brpc::StreamClose(_sd);
-    };
-    virtual void Echo(google::protobuf::RpcController* controller,
-                      const example::EchoRequest* /*request*/,
-                      example::EchoResponse* response,
-                      google::protobuf::Closure* done) {
-        // This object helps you to call done->Run() in RAII style. If you need
-        // to process the request asynchronously, pass done_guard.release().
-        brpc::ClosureGuard done_guard(done);
+  StreamingEchoService() : _sd(brpc::INVALID_STREAM_ID){};
+  virtual ~StreamingEchoService() { brpc::StreamClose(_sd); };
+  virtual void Echo(google::protobuf::RpcController *controller,
+                    const example::EchoRequest * /*request*/,
+                    example::EchoResponse *response,
+                    google::protobuf::Closure *done) {
+    // This object helps you to call done->Run() in RAII style. If you need
+    // to process the request asynchronously, pass done_guard.release().
+    brpc::ClosureGuard done_guard(done);
 
-        brpc::Controller* cntl =
-            static_cast<brpc::Controller*>(controller);
-        brpc::StreamOptions stream_options;
-        stream_options.handler = &_receiver;
-        if (brpc::StreamAccept(&_sd, *cntl, &stream_options) != 0) {
-            cntl->SetFailed("Fail to accept stream");
-            return;
-        }
-        response->set_message("Accepted stream");
+    brpc::Controller *cntl = static_cast<brpc::Controller *>(controller);
+    brpc::StreamOptions stream_options;
+    stream_options.handler = &_receiver;
+    if (brpc::StreamAccept(&_sd, *cntl, &stream_options) != 0) {
+      cntl->SetFailed("Fail to accept stream");
+      return;
     }
+    response->set_message("Accepted stream");
+  }
 
 private:
-    StreamReceiver _receiver;
-    brpc::StreamId _sd;
+  StreamReceiver _receiver;
+  brpc::StreamId _sd;
 };
 
-int main(int argc, char* argv[]) {
-    // Parse gflags. We recommend you to use gflags as well.
-    GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
+int main(int argc, char *argv[]) {
+  // Parse gflags. We recommend you to use gflags as well.
+  GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
 
-    // Generally you only need one Server.
-    brpc::Server server;
+  // Generally you only need one Server.
+  brpc::Server server;
 
-    // Instance of your service.
-    StreamingEchoService echo_service_impl;
+  // Instance of your service.
+  StreamingEchoService echo_service_impl;
 
-    // Add the service into server. Notice the second parameter, because the
-    // service is put on stack, we don't want server to delete it, otherwise
-    // use brpc::SERVER_OWNS_SERVICE.
-    if (server.AddService(&echo_service_impl, 
-                          brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
-        LOG(ERROR) << "Fail to add service";
-        return -1;
-    }
+  // Add the service into server. Notice the second parameter, because the
+  // service is put on stack, we don't want server to delete it, otherwise
+  // use brpc::SERVER_OWNS_SERVICE.
+  if (server.AddService(&echo_service_impl, brpc::SERVER_DOESNT_OWN_SERVICE) !=
+      0) {
+    LOG(ERROR) << "Fail to add service";
+    return -1;
+  }
 
-    // Start the server. 
-    brpc::ServerOptions options;
-    options.idle_timeout_sec = FLAGS_idle_timeout_s;
-    if (server.Start(FLAGS_port, &options) != 0) {
-        LOG(ERROR) << "Fail to start EchoServer";
-        return -1;
-    }
+  // Start the server.
+  brpc::ServerOptions options;
+  options.idle_timeout_sec = FLAGS_idle_timeout_s;
+  if (server.Start(FLAGS_port, &options) != 0) {
+    LOG(ERROR) << "Fail to start EchoServer";
+    return -1;
+  }
 
-    // Wait until Ctrl-C is pressed, then Stop() and Join() the server.
-    server.RunUntilAskedToQuit();
-    return 0;
+  // Wait until Ctrl-C is pressed, then Stop() and Join() the server.
+  server.RunUntilAskedToQuit();
+  return 0;
 }
